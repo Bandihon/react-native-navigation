246da09449e07dc60b6ad094c0fd1e3c49b4be4b
yogevbd <yogevbd@wix.com>
2018-03-04 12:11:31 +0200

    fixed custom top bar alignment

--- ios/RCCCustomTitleView.m
+++ ios/RCCCustomTitleView.m
@@ -29,15 +29,23 @@
         
         if ([alignment isEqualToString:@"fill"]) {
             self.frame = frame;
-            self.subView.frame = self.frame;
+            subView.sizeFlexibility = RCTRootViewSizeFlexibilityNone;
         } else {
             self.subView.delegate = self;
+            subView.sizeFlexibility = RCTRootViewSizeFlexibilityWidthAndHeight;
         }
         
         [self addSubview:subView];
     }
     
     return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    if ([self.alignment isEqualToString:@"fill"]) {
+        [self.subView setFrame:self.bounds];
+    }
 }
 
 - (void)rootViewDidChangeIntrinsicSize:(RCTRootView *)rootView {
